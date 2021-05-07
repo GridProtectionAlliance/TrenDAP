bb84f1b757f799200cf6431246eb3db3542d19f3
StephenCWills <swills@gridprotectionalliance.org>
2021-05-07 12:20:21 -0400

    Fixes and cleanup in HIDS tests.

--- HIDSTests/SimpleTests.cs
+++ HIDSTests/SimpleTests.cs
@@ -8,8 +8,8 @@ namespace HIDSTests
     [TestClass]
     public class SimpleTests
     {
-        private static string TestURL = "http://vmhidsdev:8086/";
-        private static string TestTag = "GPA.TestDevice.TestTrend";
+        private static readonly string TestURL = "http://vmhidsdev:8086/";
+        private static readonly string TestTag = "GPA.TestDevice.TestTrend";
 
         private readonly API m_api = new API();
 
@@ -26,7 +26,11 @@ namespace HIDSTests
         public void WriteTest()
         {
             TestContext.WriteLine("Starting write...");
-            m_api.WritePoints(new[] { new Point { Tag = TestTag, Minimum = 1.0D, Maximum = 10.0D, Average = 5.0D, QualityFlags = 7u, Timestamp = DateTime.UtcNow } });
+
+            Point testPoint = new Point { Tag = TestTag, Minimum = 1.0D, Maximum = 10.0D, Average = 5.0D, QualityFlags = 7u, Timestamp = DateTime.UtcNow };
+            Task writeTask = m_api.WritePointsAsync(new[] { testPoint });
+            writeTask.GetAwaiter().GetResult();
+
             TestContext.WriteLine("Write complete.");
         }
 
@@ -34,13 +38,16 @@ namespace HIDSTests
         public void ReadTest()
         {
             TestContext.WriteLine("Starting read...");
-            ReadTestAsync().Wait();
+
+            Task readTask = ReadTestAsync();
+            readTask.GetAwaiter().GetResult();
+
             TestContext.WriteLine("Read complete.");
         }
 
         private async Task ReadTestAsync()
         {
-            void BuildQuery(IQueryBuilder queryBuilder) => queryBuilder
+            static void BuildQuery(IQueryBuilder queryBuilder) => queryBuilder
                 .Range("-48h")
                 .FilterTags(new[] { TestTag });
 
