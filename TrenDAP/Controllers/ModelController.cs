834e7e3d6ebecd0b3943178507c3ed74f4608a3b
Gabriel Santos <gcsantos@gridprotectionalliance.org>
2024-05-03 09:57:09 -0400

    Addition of new attributes and support in model controller

--- TrenDAP/Controllers/ModelController.cs
+++ TrenDAP/Controllers/ModelController.cs
@@ -24,14 +24,17 @@
 using Gemstone.Data;
 using Gemstone.Data.Model;
 using Gemstone.Reflection.MemberInfoExtensions;
+using GSF.Data.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Reflection;
+using TrenDAP.Attributes;
 using TrenDAP.Model;
 
 namespace TrenDAP.Controllers
@@ -66,26 +69,9 @@ namespace TrenDAP.Controllers
         public ModelController(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
-
-        public ModelController(IConfiguration configuration, bool hasParent, string parentKey, string primaryKeyField = "ID")
-        {
-            Configuration = configuration;
-            HasParent = hasParent;
-            ParentKey = parentKey;
-            HasUniqueKey = false;
-            UniqueKeyField = "";
-            PrimaryKeyField = "ID";
-
-        }
-
-        public ModelController(IConfiguration configuration, bool hasParent, string parentKey, bool hasUniqueKey, string uniqueKey)
-        {
-            Configuration = configuration;
-            HasParent = hasParent;
-            ParentKey = parentKey;
-            HasUniqueKey = hasUniqueKey;
-            UniqueKeyField = uniqueKey;
+            PrimaryKeyField = typeof(T).GetProperties().FirstOrDefault(p => p.GetCustomAttributes<PrimaryKeyAttribute>().Any())?.Name ?? "ID";
+            ParentKey = typeof(T).GetProperties().FirstOrDefault(p => p.GetCustomAttributes<ParentKeyAttribute>().Any())?.Name ?? "";
+            CustomView = typeof(T).GetCustomAttribute<CustomViewAttribute>()?.CustomView ?? "";
         }
 
         #endregion
@@ -93,7 +79,6 @@ namespace TrenDAP.Controllers
         #region [ Properties ]
         protected IConfiguration Configuration { get; }
 
-        protected virtual bool HasParent { get; } = false;
         protected virtual string ParentKey { get; } = "";
         protected virtual string PrimaryKeyField { get; } = "ID";
         protected virtual bool HasUniqueKey { get; } = false;
@@ -103,6 +88,7 @@ namespace TrenDAP.Controllers
         protected virtual string PostRoles { get; } = "Administrator";
         protected virtual string PatchRoles { get; } = "Administrator";
         protected virtual string DeleteRoles { get; } = "Administrator";
+        protected virtual string CustomView { get; } = "";
         protected virtual string GetOrderByExpression { get; } = null;
         #endregion
 
@@ -143,18 +129,18 @@ namespace TrenDAP.Controllers
                     try
                     {
                         IEnumerable<T> result;
-                        if (HasParent && parentID != null)
+                        if (!string.IsNullOrEmpty(ParentKey) && parentID != null)
                         {
                             PropertyInfo parentKey = typeof(T).GetProperty(ParentKey);
                             if (parentKey.PropertyType == typeof(int))
-                                result = new TableOperations<T>(connection).QueryRecords(GetOrderByExpression, new RecordRestriction(ParentKey + " = {0}", int.Parse(parentID)));
+                                result = QueryRecordsWhere(null, false, ParentKey + " = {0}", int.Parse(parentID));
                             else if (parentKey.PropertyType == typeof(Guid))
-                                result = new TableOperations<T>(connection).QueryRecords(GetOrderByExpression, new RecordRestriction(ParentKey + " = {0}", Guid.Parse(parentID)));
+                                result = QueryRecordsWhere(null, false, ParentKey + " = {0}", Guid.Parse(parentID));
                             else
-                                result = new TableOperations<T>(connection).QueryRecords(GetOrderByExpression, new RecordRestriction(ParentKey + " = {0}", parentID));
+                                result = QueryRecordsWhere(null, false, ParentKey + " = {0}", parentID);
                         }
                         else
-                            result = new TableOperations<T>(connection).QueryRecords(GetOrderByExpression);
+                            result = QueryRecordsWhere(null, false, null);
 
                         return Ok(result);
                     }
@@ -182,11 +168,11 @@ namespace TrenDAP.Controllers
                         T result = null;
                         PropertyInfo primaryKey = typeof(T).GetProperty(PrimaryKeyField);
                         if (primaryKey.PropertyType == typeof(int))
-                            result = new TableOperations<T>(connection).QueryRecordWhere(PrimaryKeyField + " = {0}", int.Parse(id));
+                            result = QueryRecordWhere(PrimaryKeyField + " = {0}", int.Parse(id));
                         else if (primaryKey.PropertyType == typeof(Guid))
-                            result = new TableOperations<T>(connection).QueryRecordWhere(PrimaryKeyField + " = {0}", Guid.Parse(id));
+                            result = QueryRecordWhere(PrimaryKeyField + " = {0}", Guid.Parse(id));
                         else
-                            result = new TableOperations<T>(connection).QueryRecordWhere(PrimaryKeyField + " = {0}", id);
+                            result = QueryRecordWhere(PrimaryKeyField + " = {0}", id);
 
                         if (result == null)
                         {
@@ -214,12 +200,68 @@ namespace TrenDAP.Controllers
 
         }
 
+        protected virtual IEnumerable<T> QueryRecordsWhere(string orderBy, bool ascending, string filterExpression, params object[] parameters)
+        {
+            using (AdoDataConnection connection = new AdoDataConnection(Configuration[SettingCategory + ":ConnectionString"], Configuration[SettingCategory + ":DataProviderString"]))
+            {
+                RecordRestriction restriction = string.IsNullOrEmpty(filterExpression) ? null : new RecordRestriction(filterExpression, parameters);
+                if (string.IsNullOrEmpty(CustomView))
+                    return new TableOperations<T>(connection).QueryRecords(null, restriction);
+
+                string flt = filterExpression;
+
+                string orderString = null;
+                if (!string.IsNullOrEmpty(orderBy))
+                    orderString = orderBy + (ascending ? " ASC" : " DESC");
+
+                string sql = $@"
+                    SELECT * FROM 
+                    ({CustomView}) FullTbl 
+                    {(string.IsNullOrEmpty(filterExpression) ? "" : $"WHERE { flt}")}
+                    {(string.IsNullOrEmpty(orderBy) ? "" : " ORDER BY " + orderString)}";
+
+
+                DataTable dataTbl = connection.RetrieveData(sql, parameters);
+
+                List<T> result = new List<T>();
+                TableOperations<T> tblOperations = new TableOperations<T>(connection);
+                foreach (DataRow row in dataTbl.Rows)
+                {
+                    result.Add(tblOperations.LoadRecord(row));
+                }
+                return result;
+            }
+        }
+
+        protected virtual T QueryRecordWhere(string filterExpression, params object[] parameters)
+        {
+            using (AdoDataConnection connection = new AdoDataConnection(Configuration[SettingCategory + ":ConnectionString"], Configuration[SettingCategory + ":DataProviderString"]))
+            {
+                if (CustomView == String.Empty)
+                    return new TableOperations<T>(connection).QueryRecordWhere(filterExpression, parameters);
+
+                string whereClause = filterExpression;
+                object[] param = parameters;
+
+                whereClause = " WHERE " + whereClause;
+                string sql = "SELECT * FROM (" + CustomView + ") FullTbl";
+                DataTable dataTbl = connection.RetrieveData(sql + whereClause, param);
+
+                TableOperations<T> tblOperations = new TableOperations<T>(connection);
+                if (dataTbl.Rows.Count > 0)
+                    return tblOperations.LoadRecord(dataTbl.Rows[0]);
+                return null;
+
+            }
+        }
+
 
         [HttpPost]
         public virtual ActionResult Post([FromBody] JObject record)
         {
             try
             {
+                if (CustomView != String.Empty) return BadRequest();
                 if (PostRoles == string.Empty || User.IsInRole(PostRoles))
                 {
                     using (AdoDataConnection connection = new AdoDataConnection(Configuration[SettingCategory + ":ConnectionString"], Configuration[SettingCategory + ":DataProviderString"]))
@@ -258,6 +300,7 @@ namespace TrenDAP.Controllers
         {
             try
             {
+                if (CustomView != String.Empty) return BadRequest();
                 if (PatchRoles == string.Empty || User.IsInRole(PatchRoles))
                 {
 
@@ -287,6 +330,7 @@ namespace TrenDAP.Controllers
         {
             try
             {
+                if (CustomView != String.Empty) return BadRequest();
                 if (DeleteRoles == string.Empty || User.IsInRole(DeleteRoles))
                 {
 
