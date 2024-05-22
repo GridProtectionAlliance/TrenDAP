ccc6d82c5e3ad74d74100097b81ad4995bd4e44b
prestoncraw <prestoncrawford530@gmail.com>
2024-05-22 14:36:52 -0400

    Cleanedup UI for WorkSpaces

--- TrenDAP/wwwroot/TypeScript/Features/WorkSpaces/WorkSpaces.tsx
+++ TrenDAP/wwwroot/TypeScript/Features/WorkSpaces/WorkSpaces.tsx
@@ -25,107 +25,185 @@
 import * as React from 'react';
 import { TrenDAP, Redux } from '../../global';
 import { useAppSelector, useAppDispatch } from '../../hooks';
-import Table from '@gpa-gemstone/react-table'
-import { Sort, FetchWorkSpaces, SelectWorkSpacesStatus, RemoveWorkSpace, SelectWorkSpacesForUser, SelectWorkSpacesAllPublicNotUser, SelectWorkSpacesSortField, SelectWorkSpacesAscending } from './WorkSpacesSlice';
-import EditWorkSpace from './EditWorkSpace';
-import { TrashCan, HeavyCheckMark } from './../../Constants';
+import { Sort, FetchWorkSpaces, SelectWorkSpacesStatus, RemoveWorkSpace, SelectWorkSpacesForUser, SelectWorkSpacesAllPublicNotUser, SelectWorkSpacesSortField, SelectWorkSpacesAscending, SelectNewWorkSpace } from './WorkSpacesSlice';
+import WorkspaceSettings from './WorkspaceSettings';
 import moment from 'moment';
-import { SelectDataSets } from '../DataSets/DataSetsSlice';
-import { useNavigate } from 'react-router-dom'
+import { SelectDataSetsStatus, FetchDataSets } from '../DataSets/DataSetsSlice';
+import { useNavigate } from 'react-router-dom';
+import { ReactTable } from '@gpa-gemstone/react-table';
+import { Warning } from '@gpa-gemstone/react-interactive';
+import { ReactIcons } from '@gpa-gemstone/gpa-symbols';
 
-const WorkSpaces: React.FunctionComponent = (props: {}) => {
+const Workspaces: React.FunctionComponent = () => {
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
     const workSpaces = useAppSelector((state: Redux.StoreState) => SelectWorkSpacesForUser(state, userName));
     const publicWorkSpaces = useAppSelector((state: Redux.StoreState) => SelectWorkSpacesAllPublicNotUser(state, userName));
-    const dataSets = useAppSelector(SelectDataSets);
     const wsStatus = useAppSelector(SelectWorkSpacesStatus);
+    const dsStatus = useAppSelector(SelectDataSetsStatus);
+    const newWorkspace = useAppSelector(SelectNewWorkSpace);
 
     const sortField = useAppSelector(SelectWorkSpacesSortField);
     const ascending = useAppSelector(SelectWorkSpacesAscending);
+
+    const headerRef = React.useRef<HTMLDivElement>();
+    const [cardHeaderHeight, setCardHeaderHeight] = React.useState<number>(50);
+    const [deletedWorkSpace, setDeletedWorkSpace] = React.useState<TrenDAP.iWorkSpace>(null);
+    const [showAddModal, setShowAddModal] = React.useState<boolean>(false);
+    const [showEditModal, setShowEditModal] = React.useState<boolean>(false);
+
+    React.useLayoutEffect(() => {
+        if (cardHeaderHeight !== headerRef.current?.offsetHeight)
+            setCardHeaderHeight(headerRef.current.offsetHeight)
+    })
 
     React.useEffect(() => {
         if (wsStatus != 'unitiated' && wsStatus != 'changed') return;
         dispatch(FetchWorkSpaces());
 
-        return function () {
-        }
     }, [dispatch, wsStatus]);
 
+
+    React.useEffect(() => {
+        if (dsStatus != 'unitiated' && dsStatus != 'changed') return;
+        dispatch(FetchDataSets());
+
+    }, [dispatch, dsStatus]);
+
     return (
-        <div className="row" style={{ margin: 10 }}>
-            <div className="col-8" style={{padding: '0 0 0 0'}}>
-                <div className="card">
-                    <div className="card-header">My Workspaces</div>
-                    <div className="card-body">
-                        <Table<TrenDAP.iWorkSpace>
-                            cols={[
-                                { key: 'Name', label: 'Name', field: 'Name'},
-                                { key: 'DataSetID', label: 'Data Set',  field: 'DataSetID',content: (item, key, style) => dataSets.find(ds => ds.ID === item.DataSetID)?.Name },
-                                { key: 'Type', label: 'Type', field: 'Type' },
-
-                                { key: 'Public', label: 'Shared', field: 'Public', content: (item, key, style) => <span>{item[key] ? HeavyCheckMark : null}</span> },
-                                { key: 'UpdatedOn', field: 'UpdatedOn',label: 'Updated', content: (item, key, style) => <span>{moment(item.UpdatedOn).subtract(new Date().getTimezoneOffset(), 'minutes').format('MM/DD/YY HH:mm')}</span> },
-                                    {
-                                        key: null, label: '', content: (item, key, style) =>
-                                            <span><EditWorkSpace WorkSpace={item} /><button className="btn" onClick={(evt) => {
-                                                evt.preventDefault();
-                                                dispatch(RemoveWorkSpace(item));
-                                            }}>{TrashCan}</button></span>
-                                    }
-
-                            ]}
-                            tableClass="table table-hover"
-                            theadStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%', height: 50 }}
-                            tbodyStyle={{ display: 'block', overflowY: 'scroll', maxHeight: window.innerHeight - 215, height: window.innerHeight - 215, width: '100%' }}
-                            rowStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
-                            sortKey={sortField}
-                            onClick={(data, evt) => {
-                                if (evt.target.tagName.toLowerCase() === 'td')
-                                    navigate(`${homePath}WorkSpaceEditor/${data.row.ID}`)
-                            }}
-                            onSort={data => dispatch(Sort({ SortField: data.colField, Ascending: data.ascending }))}
-                            data={workSpaces}
-                            ascending={ascending}
-                            />
+        <div className="row">
+            <div className="col-12 col-md-6 col-xl-8">
+                <div className="card h-100">
+                    <div className="card-header" ref={headerRef}>
+                        <div className="row">
+                            <div className="d-flex col-6 justify-content-start">
+                                <h4>My Workspaces</h4>
+                            </div>
+                            <div className="d-flex col-6 justify-content-end">
+                                <button className="btn btn-primary" onClick={() => setShowAddModal(true)}> Add Workspace</button>
+                                <WorkspaceSettings New={true} Workspace={newWorkspace} Show={showAddModal} SetShow={setShowAddModal} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-body" style={{ overflow: 'hidden' }}>
+                        <div className="container-fluid d-flex h-100 flex-column">
+                            <ReactTable.Table<TrenDAP.iWorkSpace>
+                                TableClass="table table-hover"
+                                TableStyle={{
+                                    padding: 0, width: 'calc(100%)', height: '100%',
+                                    tableLayout: 'fixed', overflow: 'hidden', display: 'flex', flexDirection: 'column', marginBottom: 0
+                                }}
+                                TheadStyle={{ fontSize: 'auto', tableLayout: 'fixed', display: 'table', width: '100%' }}
+                                TbodyStyle={{ display: 'block', overflowY: 'scroll', flex: 1 }}
+                                RowStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
+                                SortKey={sortField}
+                                OnClick={(data, evt) => {
+                                    if (evt.target.tagName.toLowerCase() === 'td')
+                                        navigate(`${homePath}WorkSpaceEditor/${data.row.ID}`)
+                                }}
+                                OnSort={data => dispatch(Sort({ SortField: data.colField, Ascending: data.ascending }))}
+                                Data={workSpaces}
+                                Ascending={ascending}
+                                KeySelector={(row) => row.ID}
+                            >
+                                <ReactTable.Column<TrenDAP.iWorkSpace>
+                                    Key={'Name'}
+                                    AllowSort={true}
+                                    Field={'Name'}
+                                >
+                                    Name
+                                </ReactTable.Column>
+                                <ReactTable.Column<TrenDAP.iWorkSpace>
+                                    Key={'Public'}
+                                    AllowSort={true}
+                                    Field={'Public'}
+                                    Content={(row) => <span>{row.item[row.key] ? <ReactIcons.CheckMark Color="green"/> : null}</span>}
+                                >
+                                    Shared
+                                </ReactTable.Column>
+                                <ReactTable.Column<TrenDAP.iWorkSpace>
+                                    Key={'UpdatedOn'}
+                                    AllowSort={true}
+                                    Field={'UpdatedOn'}
+                                    Content={(row) => <span>{moment(row.item.UpdatedOn).subtract(new Date().getTimezoneOffset(), 'minutes').format('MM/DD/YY HH:mm')}</span>}
+                                >
+                                    Updated
+                                </ReactTable.Column>
+                                <ReactTable.Column<TrenDAP.iWorkSpace>
+                                    Key={'Buttons'}
+                                    AllowSort={false}
+                                    Content={(row) =>
+                                        <>
+                                            <span>
+                                                <button className="btn" onClick={() => setShowEditModal(true)}><ReactIcons.Pencil /></button>
+                                                <WorkspaceSettings Workspace={row.item} New={false} Show={showEditModal} SetShow={setShowEditModal} />
+                                                <button className="btn" onClick={() => setDeletedWorkSpace(row.item)}><ReactIcons.TrashCan Color="red"/></button>
+                                            </span>
+                                        </>
+                                    }>
+                                    {'\u200B'}
+                                </ReactTable.Column>
+                            </ReactTable.Table>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="col-4" style={{ padding: '0 0 0 0' }}>
-                <div className="card ">
-                    <div className="card-header">Shared Workspaces</div>
-                    <div className="card-body">
-                        <Table<TrenDAP.iWorkSpace>
-                            cols={[
-                                { key: 'Name', label: 'Name', field: 'Name' },
-                                { key: 'DataSetID', field: 'DataSetID', label: 'Data Set', content: (item, key, style) => dataSets.find(ds => ds.ID === item.ID)?.Name },
-                                { key: 'UpdatedOn', field: 'UpdatedOn', label: 'Updated', content: (item, key, style) => <span>{moment(item.UpdatedOn).subtract(new Date().getTimezoneOffset(), 'minutes').format('MM/DD/YY HH:mm')}</span> },
-
-                                //{ key: null, label: '', content: (item, key, style) => <span><EditWorkSpace WorkSpace={item} /><button className="btn" onClick={() => dispatch(RemoveWorkSpace(item))}>{TrashCan}</button></span> }
-
-                            ]}
-                            tableClass="table table-hover"
-                            theadStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%', height: 50 }}
-                            tbodyStyle={{ display: 'block', overflowY: 'scroll', maxHeight: window.innerHeight - 215, height: window.innerHeight - 215, width: '100%' }}
-                            rowStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
-                            sortKey={sortField}
-                            onClick={(data, evt) => {
-                                if (evt.target.tagName.toLowerCase() === 'td')
-                                    navigate(`${homePath}WorkSpaceEditor/${data.row.ID}`)
+            <div className="d-none d-md-block col-6 col-xl-4">
+                <div className="card h-100">
+                    <div className="card-header">
+                        <h4>Shared Workspaces</h4>
+                    </div>
+                    <div className="card-body" style={{ overflow: 'hidden' }}>
+                        <div className="container-fluid d-flex h-100 flex-column">
+                            <ReactTable.Table<TrenDAP.iWorkSpace>
+                                TableClass="table table-hover"
+                                TableStyle={{
+                                    padding: 0, width: 'calc(100%)', height: '100%',
+                                    tableLayout: 'fixed', overflow: 'hidden', display: 'flex', flexDirection: 'column', marginBottom: 0
+                                }}
+                                TheadStyle={{ fontSize: 'auto', tableLayout: 'fixed', display: 'table', width: '100%' }}
+                                TbodyStyle={{ display: 'block', overflowY: 'scroll', flex: 1 }}
+                                RowStyle={{ fontSize: 'smaller', display: 'table', tableLayout: 'fixed', width: '100%' }}
+                                SortKey={sortField}
+                                OnClick={(data, evt) => {
+                                    if (evt.target.tagName.toLowerCase() === 'td')
+                                        navigate(`${homePath}WorkSpaceEditor/${data.row.ID}`)
+                                }}
+                                OnSort={data => dispatch(Sort({ SortField: data.colField, Ascending: data.ascending }))}
+                                Data={publicWorkSpaces}
+                                Ascending={ascending}
+                                KeySelector={row => row.ID}
+                            >
+                                <ReactTable.Column<TrenDAP.iWorkSpace>
+                                    Key={'Name'}
+                                    AllowSort={true}
+                                    Field={'Name'}
+                                >
+                                    Name
+                                </ReactTable.Column>
+                                <ReactTable.Column<TrenDAP.iWorkSpace>
+                                    Key={'UpdatedOn'}
+                                    AllowSort={true}
+                                    Field={'UpdatedOn'}
+                                    Content={(row) => <span>{moment(row.item.UpdatedOn).subtract(new Date().getTimezoneOffset(), 'minutes').format('MM/DD/YY HH:mm')}</span>}
+                                >
+                                    Updated
+                                </ReactTable.Column>
+                            </ReactTable.Table>
+                        </div>
+                        <Warning Title={'Delete ' + deletedWorkSpace?.Name} Show={deletedWorkSpace != null} Message={"This will remove the workspace and can not be undone."}
+                            CallBack={(c) => {
+                                if (c)
+                                    dispatch(RemoveWorkSpace(deletedWorkSpace));
+                                setDeletedWorkSpace(null);
                             }}
-                            onSort={data => dispatch(Sort({ SortField: data.colField, Ascending: data.ascending }))}
-                            data={publicWorkSpaces}
-                            ascending={ascending}
                         />
-
                     </div>
                 </div>
             </div>
-
         </div>
-
     );
 }
 
-export default WorkSpaces;
+export default Workspaces;
